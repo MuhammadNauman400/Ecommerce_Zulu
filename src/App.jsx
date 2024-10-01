9e8b1d36d2372a70d3a6672bfc05bb8a77fcc04c
MuhammadNauman400 <naumanmuhammad663@gmail.com>
2024-10-02 00:15:44 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
-import About from './pages/about';
-import Contact from './pages/contact';
-import Product from './pages/product';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Product from './pages/Product';
 import Cart from './pages/Cart';
 import Login from './pages/Login';
 import Navbar from './components/Navbar';
